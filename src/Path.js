32171cc57798024c351e4df7d2c8656f8e981aad
TerryHintz <cza82@sfu.ca>
2020-05-29 17:36:06 -0700

    setup grid

--- src/Path.js
+++ src/Path.js
@@ -1,10 +1,47 @@
 import React, {Component} from 'react';
 import './Path.css'
 
+const HEIGHT = 9;
+const WIDTH = 16;
+
 class Path extends Component {
+    state = {
+        grid: [],
+    }
+
+    componentDidMount(){
+        this.createGrid();
+    }
+
+    createGrid = () => {
+        let grid = [];
+        for(let i=0; i<HEIGHT; i++){
+            let row = [];
+            for(let j=0; j<WIDTH; j++){
+                row.push(null);
+            }
+            grid.push(row);
+        }
+        this.setState({grid});
+    }
+
     render () {
         return (
-            <div>path</div>
+            <div className='path-root'>
+                <div className='path-grid'>
+                    {this.state.grid.map((row, index) => {
+                        return(
+                            <div key={index} className='path-row'>
+                                {row.map((square, index) => {
+                                    return (
+                                        <div key={index} className='path-square'></div>
+                                    )
+                                })}
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
         )
     }
 }
