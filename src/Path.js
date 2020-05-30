b818b5898c68b04c84000e178fbc91609cf88ba7
TerryHintz <cza82@sfu.ca>
2020-05-29 17:11:05 -0700

    setup path

---
+++ src/Path.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './Path.css'
+
+class Path extends Component {
+    render () {
+        return (
+            <div>path</div>
+        )
+    }
+}
+
+export default Path;
